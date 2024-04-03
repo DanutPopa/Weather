4b3ec78d285451533079c676edc8c427eab01b47
DanutPopa <popa.ion.danut@gmail.com>
2024-04-03 18:09:06 +0300

    Initial Commit

---
+++ Weather/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Danut Popa on 03.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
