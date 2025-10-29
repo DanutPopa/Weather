9dd31a65e22431d0c31daa6c439db2b40d5718fb
Danut Popa <popa.ion.danut@gmail.com>
2025-10-29 18:53:20 +0200

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Danut Popa on 29.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
